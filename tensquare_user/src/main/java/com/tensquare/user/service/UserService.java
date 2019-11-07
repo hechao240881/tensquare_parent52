251b7cad0190567ad995ca763949b7f0c90e5222
Ms`chao <hc240881@outlook.com>
2019-11-07 23:20:18 +0800

    添加 user 模块 增加注册功能 增加手机验证码

---
+++ tensquare_user/src/main/java/com/tensquare/user/service/UserService.java
@@ -0,0 +1,190 @@
+package com.tensquare.user.service;
+
+import com.tensquare.user.dao.UserDao;
+import com.tensquare.user.pojo.User;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import util.IdWorker;
+
+import javax.persistence.criteria.Predicate;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 服务层
+ *
+ * @author Administrator
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private IdWorker idWorker;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+
+
+	public void sendSms(String mobile){
+		//生成六位随机数
+        String checkCode = RandomStringUtils.randomNumeric(6);
+        //向缓存中放一份,K值为生成的六位随机数和手机号码，6小时内有效
+        redisTemplate.opsForValue().set("checkCode_"+mobile, checkCode,6,TimeUnit.HOURS);
+        //给用户发一份
+        Map<String, String> map = new HashMap<>();
+        map.put("mobile",mobile);
+        map.put("checkCode",checkCode);
+        rabbitTemplate.convertAndSend("sms",map);
+        //在控制台显示一份（方便测试）
+        System.out.println("验证码为：" + checkCode);
+    }
+
+
+	/**
+     * 查询全部列表
+     *
+     * @return
+     */
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+
+    /**
+     * 条件查询+分页
+     *
+     * @param whereMap
+     * @param page
+     * @param size
+     * @return
+     */
+    public Page<User> findSearch(Map whereMap, int page, int size) {
+        Specification<User> specification = createSpecification(whereMap);
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
+        return userDao.findAll(specification, pageRequest);
+    }
+
+
+    /**
+     * 条件查询
+     *
+     * @param whereMap
+     * @return
+     */
+    public List<User> findSearch(Map whereMap) {
+        Specification<User> specification = createSpecification(whereMap);
+        return userDao.findAll(specification);
+    }
+
+    /**
+     * 根据ID查询实体
+     *
+     * @param id
+     * @return
+     */
+    public User findById(String id) {
+        return userDao.findById(id).get();
+    }
+
+    /**
+     * 增加
+     *
+     * @param user
+     */
+    public void add(User user) {
+        user.setId(idWorker.nextId() + "");
+        user.setId(idWorker.nextId()+"");
+        user.setFollowcount(0);//关注数
+        user.setFanscount(0);//粉丝数
+        user.setOnline(0L);//在线时长
+        user.setRegdate(new Date());//注册日期
+        user.setUpdatedate(new Date());//更新日期
+        user.setLastdate(new Date());//最后登入
+        userDao.save(user);
+    }
+
+    /**
+     * 修改
+     *
+     * @param user
+     */
+    public void update(User user) {
+        userDao.save(user);
+    }
+
+    /**
+     * 删除
+     *
+     * @param id
+     */
+    public void deleteById(String id) {
+        userDao.deleteById(id);
+    }
+
+    /**
+     * 动态条件构建
+     *
+     * @param searchMap
+     * @return
+     */
+    private Specification<User> createSpecification(Map searchMap) {
+
+        return (Specification<User>) (root, query, cb) -> {
+            List<Predicate> predicateList = new ArrayList<Predicate>();
+			// ID
+            if (searchMap.get("id") != null && !"".equals(searchMap.get("id"))) {
+                predicateList.add(cb.like(root.get("id").as(String.class), "%" + (String) searchMap.get("id") + "%"));
+            }
+			// 手机号码
+            if (searchMap.get("mobile") != null && !"".equals(searchMap.get("mobile"))) {
+                predicateList.add(cb.like(root.get("mobile").as(String.class), "%" + (String) searchMap.get("mobile") + "%"));
+            }
+			// 密码
+            if (searchMap.get("password") != null && !"".equals(searchMap.get("password"))) {
+                predicateList.add(cb.like(root.get("password").as(String.class), "%" + (String) searchMap.get("password") + "%"));
+            }
+			// 昵称
+            if (searchMap.get("nickname") != null && !"".equals(searchMap.get("nickname"))) {
+                predicateList.add(cb.like(root.get("nickname").as(String.class), "%" + (String) searchMap.get("nickname") + "%"));
+            }
+			// 性别
+            if (searchMap.get("sex") != null && !"".equals(searchMap.get("sex"))) {
+                predicateList.add(cb.like(root.get("sex").as(String.class), "%" + (String) searchMap.get("sex") + "%"));
+            }
+			// 头像
+            if (searchMap.get("avatar") != null && !"".equals(searchMap.get("avatar"))) {
+                predicateList.add(cb.like(root.get("avatar").as(String.class), "%" + (String) searchMap.get("avatar") + "%"));
+            }
+			// E-Mail
+            if (searchMap.get("email") != null && !"".equals(searchMap.get("email"))) {
+                predicateList.add(cb.like(root.get("email").as(String.class), "%" + (String) searchMap.get("email") + "%"));
+            }
+			// 兴趣
+            if (searchMap.get("interest") != null && !"".equals(searchMap.get("interest"))) {
+                predicateList.add(cb.like(root.get("interest").as(String.class), "%" + (String) searchMap.get("interest") + "%"));
+            }
+			// 个性
+            if (searchMap.get("personality") != null && !"".equals(searchMap.get("personality"))) {
+                predicateList.add(cb.like(root.get("personality").as(String.class), "%" + (String) searchMap.get("personality") + "%"));
+            }
+
+            return cb.and(predicateList.toArray(new Predicate[predicateList.size()]));
+
+        };
+
+    }
+
+}
